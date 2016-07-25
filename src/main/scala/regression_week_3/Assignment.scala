e9aedc5ef7f92e0d276ff6f13fd9343430051657
cnnickolay <nickolayc@gmail.com>
2016-07-25 22:38:05 +0100

    implementing loss calculation

--- src/main/scala/regression_week_3/Assignment.scala
+++ src/main/scala/regression_week_3/Assignment.scala
@@ -1,11 +1,13 @@
 package regression_week_3
 
+import java.lang.Math
+
 import com.quantifind.charts.Highcharts
 import com.quantifind.charts.Highcharts._
 import com.quantifind.charts.highcharts.{Exporting, Zoom, _}
 import com.quantifind.charts.repl.IterablePair
 import org.apache.spark.mllib.linalg.Vectors
-import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionWithSGD, RidgeRegressionWithSGD}
+import org.apache.spark.mllib.regression.{LabeledPoint, LinearRegressionModel, LinearRegressionWithSGD, RidgeRegressionWithSGD}
 import org.apache.spark.sql.{Column, DataFrame, Row, SQLContext}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
@@ -82,38 +84,88 @@ object Assignment {
     def extractDoubleList(df: DataFrame, column: String): List[Double] = df.rdd.map(_.getAs[Double]("column")).collect().toList
 
     def buildPlot(initialDataFrame: DataFrame, labeledPoint: (DataFrame => RDD[LabeledPoint]),
-                  degree: Int, numIterations: Int, stepSize: Double, transformationColumn: String,
-                  feature: String, targetColumn: String, graphicXColumn: String) = {
+                  degree: Int, initialStep: Double, transformationColumn: String,
+                  renderGraph: (DataFrame => Unit)) = {
       val poly1Data = polynomialDataFrame(initialDataFrame, transformationColumn, degree)
       val poly1DataLabeledPointsRDD = labeledPoint(poly1Data)
       val poly1DataModelConfig = new LinearRegressionWithSGD().setIntercept(true)
-      poly1DataModelConfig.optimizer.setNumIterations(numIterations).setStepSize(stepSize)
-      val poly1DataModel = poly1DataModelConfig.run(poly1DataLabeledPointsRDD)
-      println(s"${poly1DataModel.intercept} ${poly1DataModel.weights}")
+      poly1DataModelConfig.optimizer.setNumIterations(200)
 
-      val calculatePrediction = udf[Double, Double](sqftLiving => poly1DataModel.intercept + poly1DataModel.weights(0) * sqftLiving)
-      val poly1DataAugmented = poly1Data.withColumn(s"${targetColumn}_predicted", calculatePrediction(poly1Data(feature)))
+      def findApproximation(prevModel: Option[LinearRegressionModel], step: Double): LinearRegressionModel = {
+        poly1DataModelConfig.optimizer.setStepSize(step)
+        val model = poly1DataModelConfig.run(poly1DataLabeledPointsRDD)
+        prevModel match {
+          case Some(_prevModel) => println(s"step $step prevmodel weight ${_prevModel.weights(0)}, nextmodel weight ${model.weights(0)}")
+          case None => println(s"step $step nextmodel weight ${model.weights(0)}")
+        }
+
+        prevModel match {
+          case Some(_prevModel) if step < 1E-100 => _prevModel
+          case Some(_prevModel) if _prevModel.weights.toArray.product.isNaN && model.weights.toArray.product.isNaN => findApproximation(Some(model), step / 10)
+          case Some(_prevModel) if !_prevModel.weights.toArray.product.isNaN && model.weights.toArray.product.isNaN => _prevModel
+          case Some(_prevModel) if _prevModel.weights.toArray.product.isNaN && !model.weights.toArray.product.isNaN => findApproximation(Some(model), step / 10)
+          case Some(_prevModel) =>
+            val firstRow = poly1DataLabeledPointsRDD.first()
+            val calculateRMSE: ((LinearRegressionModel, RDD[LabeledPoint]) => Double) = {
+              (model, points) =>
+                val predicted = model.predict(points.map(_.features)).collect()
+                val averageError = (predicted, points.map(_.label).collect()).zipped.map {
+                  case (predict, observation) => Math.pow(predict - observation, 2)
+                }.sum / points.count()
+                Math.sqrt(averageError)
+            }
+            val prevModelError = calculateRMSE(_prevModel, poly1DataLabeledPointsRDD)
+            val modelError = calculateRMSE(model, poly1DataLabeledPointsRDD)
+            if (prevModelError.isNaN && modelError.isNaN) findApproximation(Some(model), step / 10)
+            else if (prevModelError.isNaN && !modelError.isNaN) findApproximation(Some(model), step / 10)
+            else if (!prevModelError.isNaN && modelError.isNaN) _prevModel
+            else if (prevModelError.isInfinity || modelError.isInfinity) findApproximation(Some(model), step / 10)
+            else if (prevModelError > modelError) {
+              println(s"label ${firstRow.label}, prev predict error $prevModelError, next predict error $modelError")
+              findApproximation(Some(model), step / 10)
+            } else _prevModel
+          case None => findApproximation(Some(model), step / 10)
+        }
+      }
+      val model = findApproximation(None, initialStep)
+
+      val featuresGrouped = poly1DataLabeledPointsRDD.collect().groupBy(_.features(0))
+      val calculatePrediction = udf[Double, Double](power1 => model.predict(featuresGrouped(power1)(0).features))
+      val poly1DataAugmented = poly1Data.withColumn(s"predicted", calculatePrediction(poly1Data("power_1")))
 
       poly1DataAugmented.show()
 
-      val series1: Series = Series(poly1DataAugmented.select(graphicXColumn, targetColumn).collect().zipWithIndex.collect { case (row, idx) if idx % 100 == 0 => Data(row.getAs[Double](0), row.getAs[Double](1)) }, chart = Some(SeriesType.scatter))
-      val series2: Series = Series(poly1DataAugmented.select(graphicXColumn, s"${targetColumn}_predicted").collect().zipWithIndex.collect { case (row, idx) if idx % 100 == 0 => Data(row.getAs[Double](0), row.getAs[Double](1)) }, chart = Some(SeriesType.scatter))
+      renderGraph(poly1DataAugmented)
+      poly1DataAugmented
+    }
 
-      val chart = Highchart(Seq(series1, series2), chart = Some(Chart(zoomType = Some(Zoom.xy))), yAxis = None)
-      plot(chart)
+    def renderPlot(title: String, xAxis: String)(df: DataFrame): Unit = {
+        val series1: Series = Series(df.select(xAxis, "price").collect().zipWithIndex.collect { case (row, idx) if idx % 100 == 0 => Data(row.getAs[Double](0), row.getAs[Double](1)) }, chart = Some(SeriesType.scatter))
+        val series2: Series = Series(df.select(xAxis, "predicted").collect().zipWithIndex.collect { case (row, idx) if idx % 100 == 0 => Data(row.getAs[Double](0), row.getAs[Double](1)) }, chart = Some(SeriesType.scatter))
+
+        val chart = Highchart(Seq(series1, series2), chart = Some(Chart(zoomType = Some(Zoom.xy))), yAxis = None, title = Some(Title(title)))
+        plot(chart)
     }
 
     buildPlot(
       kcHouseData.select("sqft_living", "price"),
       df => df.select("price", "sqft_living").rdd.map(row => LabeledPoint(row.getAs[Double](0), Vectors.dense(row.getAs[Double](1)))),
-      1, 50, 0.0000001, "sqft_living", "sqft_living", "price", "sqft_living")
-    buildPlot(
-      kcHouseData.select("sqft_living", "price"),
-      df => df.select("price", "power_2").rdd.map(row => LabeledPoint(row.getAs[Double](0), Vectors.dense(row.getAs[Double](1)))),
-      2, 1000, 0.00000000000001, "sqft_living", "power_2", "price", "sqft_living")
-    buildPlot(
-      kcHouseData.select("sqft_living", "price"),
-      df => df.select("price", "power_3").rdd.map(row => LabeledPoint(row.getAs[Double](0), Vectors.dense(row.getAs[Double](1)))),
-      3, 10000, 0.000000000000000000001, "sqft_living", "power_3", "price", "sqft_living")
+      1, 1E-5, "sqft_living", renderPlot("1st order", "sqft_living"))
+//    buildPlot(
+//      kcHouseData.select("sqft_living", "price"),
+//      df => df.select("price", "power_1", "power_2").rdd.map(row => LabeledPoint(row.getAs[Double](0), Vectors.dense(row.getAs[Double](1), row.getAs[Double](2)))),
+//      2, 1E-10, "sqft_living", renderPlot("2nd order", "sqft_living"))
+//    buildPlot(
+//      kcHouseData.select("sqft_living", "price"),
+//      df => df.select("price", "power_1", "power_2", "power_3").rdd.map(row => LabeledPoint(row.getAs[Double](0), Vectors.dense(row.getAs[Double](1), row.getAs[Double](2), row.getAs[Double](3)))),
+//      3, 1E-10, "sqft_living", renderPlot("3rd order", "sqft_living"))
+//    buildPlot(
+//      kcHouseData.select("sqft_living", "price"),
+//      df => df.select("price", "power_1", "power_2", "power_3", "power_4").rdd.map(row => LabeledPoint(row.getAs[Double](0), Vectors.dense(row.getAs[Double](1), row.getAs[Double](2), row.getAs[Double](3), row.getAs[Double](4)))),
+//      4, 1E-25, "sqft_living", renderPlot("4rd order", "sqft_living"))
+//    buildPlot(
+//      kcHouseData.select("sqft_living", "price"),
+//      df => df.select("price", "power_1", "power_2", "power_3", "power_4", "power_5").rdd.map(row => LabeledPoint(row.getAs[Double](0), Vectors.dense(row.getAs[Double](1), row.getAs[Double](2), row.getAs[Double](3), row.getAs[Double](4), row.getAs[Double](5)))),
+//      5, 1E-30, "sqft_living", renderPlot("5rd order", "sqft_living"))
   }
 }
