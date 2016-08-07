b86c71caaf75521d6e4767183507e9e453509ea2
Nikolay Cherkezishvili <ncherkezishvili@nxsystemsinc.com>
2016-08-07 23:46:01 +0100

    trying different models

--- src/main/scala/experiments/CurrencyRegression.scala
+++ src/main/scala/experiments/CurrencyRegression.scala
@@ -16,17 +16,17 @@ import scala.annotation.tailrec
 
 
 object CurrencyRegression {
-  def renderPlot(title: String, xAxis: String, yAxis: String, predictedYAxis: String, filterRatio: Int)(df: DataFrame): Unit = {
-    val series1: Series = Series(name = Some("observations"), data = df.select(xAxis, yAxis).collect().zipWithIndex.collect { case (row, idx) if idx % filterRatio == 0 => Data(row.getAs[Double](0), row.getAs[Double](1)) }, chart = Some(SeriesType.scatter))
-    val series2: Series = Series(name = Some("prediction"), data = df.select(xAxis, predictedYAxis).collect().zipWithIndex.collect { case (row, idx) if idx % filterRatio == 0 => Data(row.getAs[Double](0), row.getAs[Double](1)) }, chart = Some(SeriesType.scatter))
+  def renderPlot(filterRatio: Int, title: String, xAxis: String, yAxis: String, predictedYAxis: String*)(df: DataFrame): Unit = {
+    val observationSeries: Series = Series(name = Some("observations"), data = df.select(xAxis, yAxis).collect().zipWithIndex.collect { case (row, idx) if idx % filterRatio == 0 => Data(row.getAs[Double](0), row.getAs[Double](1)) }, chart = Some(SeriesType.scatter))
+    val predictedSeries: List[Series] = predictedYAxis.map{ predicted => Series(name = Some(predicted), data = df.select(xAxis, predicted).collect().zipWithIndex.collect { case (row, idx) if idx % filterRatio == 0 => Data(row.getAs[Double](0), row.getAs[Double](1)) }, chart = Some(SeriesType.scatter)) }.toList
 
-    val chart = Highchart(Seq(series1, series2), chart = Some(Chart(zoomType = Some(Zoom.xy))), yAxis = None, title = Some(Title(title)))
+    val chart = Highchart(predictedSeries ::: observationSeries :: Nil, chart = Some(Chart(zoomType = Some(Zoom.xy))), yAxis = None, title = Some(Title(title)))
     plot(chart)
   }
 
 
   def main(args: Array[String]) {
-    val csv = "/Users/niko/Downloads/HISTDATA_COM_ASCII_EURUSD_T201512/DAT_ASCII_EURUSD_T_201512_10000.csv"
+    val csv = "DAT_ASCII_EURUSD_T_201512.csv"
 
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local[6]")
     val sc = new SparkContext(conf)
@@ -37,46 +37,48 @@ object CurrencyRegression {
       StructField("label", DoubleType, nullable = false)
     ))
 
-    val rawDf = sqlContext.read
+    val _rawDf = sqlContext.read
       .format("com.databricks.spark.csv")
       .option("header", "false")
       .schema(customSchema)
       .load(csv)
 
-    val dateFormatter = new SimpleDateFormat("yyyyMMdd HHmmssSSS")
+    List("01", "02", "03", "04", "07", "08", "09", "10", "11",  "14", "15", "16", "17", "18", "21", "22", "23", "24", "25", "28", "29", "30", "31")
+      .foreach { value =>
+        val rawDf = _rawDf.filter(row => row.getAs[String]("date").startsWith(s"201512$value"))
 
-    val timeOffset = dateFormatter.parse(rawDf.first().getAs[String]("date")).getTime
-    val normalizeDate = udf[Double, String](rawDate => dateFormatter.parse(rawDate).getTime - timeOffset)
-    val powerValue = udf[Double, Double, Int]((rawDate, power) => Math.pow(rawDate, power))
-    val dfProcessed = rawDf
-      .withColumn("date_norm", normalizeDate(rawDf("date")))
+        val dateFormatter = new SimpleDateFormat("yyyyMMdd HHmmssSSS")
 
-    @tailrec def generatePolynomial(df: DataFrame, currentDegree: Int, maxDegree: Int): DataFrame = {
-      if (currentDegree <= maxDegree) generatePolynomial(df.withColumn(s"power_$currentDegree", powerValue(dfProcessed("date_norm"), lit(currentDegree))), currentDegree + 1, maxDegree)
-      else df
-    }
+        val timeOffset = dateFormatter.parse(rawDf.first().getAs[String]("date")).getTime
+        val normalizeDate = udf[Double, String](rawDate => Math.round((dateFormatter.parse(rawDate).getTime - timeOffset)/(1000*60)))
+        val powerValue = udf[Double, Double, Int]((rawDate, power) => Math.pow(rawDate, power))
+        val dfProcessed = rawDf.withColumn("date_norm", normalizeDate(rawDf("date")))
 
-    val df = generatePolynomial(dfProcessed, 1, 10)
-    df.show()
+        @tailrec def generatePolynomial(df: DataFrame, currentDegree: Int, maxDegree: Int): DataFrame = {
+          if (currentDegree <= maxDegree) generatePolynomial(df.withColumn(s"power_$currentDegree", powerValue(dfProcessed("date_norm"), lit(currentDegree))), currentDegree + 1, maxDegree)
+          else df
+        }
 
-    val assembler = new VectorAssembler()
-      .setInputCols(Array("power_1", "power_2", "power_3", "power_4", "power_5", "power_6", "power_7", "power_8", "power_9", "power_10"))
-      .setOutputCol("features")
+        val df = generatePolynomial(dfProcessed, 1, 20)
+        df.show()
 
-    val dfPrepared = assembler.transform(df)
+        val assembler = new VectorAssembler()
 
-    val lr = new LinearRegression()
-      .setMaxIter(100)
-      .setRegParam(0)
-      .setElasticNetParam(1)
-      .setFitIntercept(true)
+        val dfPrepared1 = assembler.setInputCols((1 to 10).map(i => s"power_$i").toArray).setOutputCol("features1").transform(df)
+        val dfPrepared2 = assembler.setInputCols((1 to 5).map(i => s"power_$i").toArray).setOutputCol("features2").transform(dfPrepared1)
 
-    val model = lr.fit(dfPrepared)
-    val dfAugmented = model.transform(dfPrepared)
+        val lr = new LinearRegression()
+          .setMaxIter(100)
+          .setRegParam(0)
+          .setElasticNetParam(0)
+          .setFitIntercept(true)
 
-    dfAugmented.show()
+        val model1 = lr.setFeaturesCol("features1").setPredictionCol("prediction1").fit(dfPrepared2)
+        val model2 = lr.setFeaturesCol("features2").setPredictionCol("prediction2").fit(dfPrepared2)
+        val dfAugmented1 = model1.transform(dfPrepared2)
+        val dfAugmented2 = model2.transform(dfAugmented1)
 
-    renderPlot("test", "date_norm", "label", "prediction", 20)(dfAugmented)
-
+        renderPlot(100, value, "date_norm", "label", "prediction1", "prediction2")(dfAugmented2)
+      }
   }
 }
